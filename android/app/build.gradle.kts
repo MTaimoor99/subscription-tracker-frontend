0223d5c8072be54bd6dcc75092300b557e4af0cd
Muhammad Taimoor <taimoor992002@gmail.com>
2026-07-22 13:42:39 +0500

    changed the application id for subsnotifier

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.subscription_tracker_frontend"
+        applicationId = "com.taimoor.subsnotifier"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
